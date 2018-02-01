4cba758cc11276cb3aa225a5b2b0d6898a53e44c
aulate <aulate@soin.co.cr>
2018-02-01 11:22:52 -0600

    First commit

--- app/src/main/java/com/effeta/miparroquiaandroid/MainActivity.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/MainActivity.kt
@@ -9,16 +9,16 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_announcements -> {
+                message.setText(R.string.title_announcements)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_eucharist -> {
+                message.setText(R.string.title_eucharist)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_church_map -> {
+                message.setText(R.string.title_church_map)
                 return@OnNavigationItemSelectedListener true
             }
         }
