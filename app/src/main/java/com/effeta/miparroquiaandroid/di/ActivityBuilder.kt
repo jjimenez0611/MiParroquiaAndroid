7f28bf44f8717f9ce1db2c4ea204fa9e586510f0
aulate <aulate@soin.co.cr>
2018-02-16 11:57:24 -0600

    Adding Dagger

---
+++ app/src/main/java/com/effeta/miparroquiaandroid/di/ActivityBuilder.kt
@@ -0,0 +1,10 @@
+package com.effeta.miparroquiaandroid.di
+
+import dagger.Module
+
+/**
+ * Created by aulate on 16/2/18.
+ */
+@Module
+abstract class ActivityBuilder {
+}
---
+++ app/src/main/java/com/effeta/miparroquiaandroid/di/AppComponent.kt
@@ -0,0 +1,33 @@
+package com.effeta.miparroquiaandroid.di
+
+import android.app.Application
+import com.effeta.miparroquiaandroid.MiParroquiaApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import dagger.android.DaggerApplication
+import javax.inject.Singleton
+
+/**
+ * Created by aulate on 16/2/18.
+ */
+@Singleton
+@Component(modules = [
+    (AndroidSupportInjectionModule::class),
+    (AppModule::class),
+    (ActivityBuilder::class)
+])
+interface AppComponent : AndroidInjector<DaggerApplication> {
+    fun inject(app: MiParroquiaApplication)
+
+    override fun inject(instance: DaggerApplication?)
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
