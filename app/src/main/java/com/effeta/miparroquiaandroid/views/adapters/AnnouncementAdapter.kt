cc673117d1886032cf60818db7f91f6b41022734
aulate <aulate@soin.co.cr>
2018-02-22 16:58:53 -0600

    Background differs when announcement type is different.

--- app/src/main/java/com/effeta/miparroquiaandroid/views/adapters/AnnouncementAdapter.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/views/adapters/AnnouncementAdapter.kt
@@ -19,6 +19,11 @@ object AnnouncementAdapter : BaseAdapter<Announcement, AnnouncementAdapter.Annou
 
     class AnnouncementViewHolder(itemView: View?) : BaseViewHolder<Announcement>(itemView) {
         override fun showItem(item: Announcement) {
+            if (item.mType.toInt() == 1)
+                itemView.setBackgroundResource(R.drawable.bg_parish_announcement)
+            else
+                itemView.setBackgroundResource(R.drawable.bg_filial_announcement)
+
             itemView.item_title.text = item.mTitle
             itemView.item_description.text = item.mDescription
             itemView.item_published_at.text = item.mPublishedAt.toString("dd-MM-yyyy")
