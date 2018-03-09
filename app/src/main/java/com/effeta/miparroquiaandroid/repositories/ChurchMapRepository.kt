821bffd9d129cc61e89236404c62835411f524c3
Jose Andres Jimenez Porras <jjimenez@soin.co.cr>
2018-03-09 16:50:51 -0600

    add filter to map

--- app/src/main/java/com/effeta/miparroquiaandroid/repositories/ChurchMapRepository.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/repositories/ChurchMapRepository.kt
@@ -2,16 +2,21 @@ package com.effeta.miparroquiaandroid.repositories
 
 import com.effeta.miparroquiaandroid.models.Church
 import com.effeta.miparroquiaandroid.services.firebase.FirebaseChurchMap
+import com.effeta.miparroquiaandroid.services.sharedPref.SharedPreferencesHelper
 import io.reactivex.Observable
 import javax.inject.Inject
 
 /**
  * Created by jjimenez on 2/26/18.
  */
-class ChurchMapRepository @Inject constructor(private val mChurchMapFirebaseChurchMap: FirebaseChurchMap) {
+class ChurchMapRepository @Inject constructor(private val mChurchMapFirebaseChurchMap: FirebaseChurchMap,
+                                              private val mSharedPreferences: SharedPreferencesHelper) {
 
 
-    fun getChurches() : Observable<List<Church>>{
-        return mChurchMapFirebaseChurchMap.getAllChurches()
+    fun getChurches(): Observable<List<Church>> {
+        val parishKey = mSharedPreferences.getParishKey()
+
+        return mChurchMapFirebaseChurchMap.getAnnouncementListByParish(parishKey)
     }
+
 }
--- app/src/main/java/com/effeta/miparroquiaandroid/services/firebase/FirebaseChurchMap.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/services/firebase/FirebaseChurchMap.kt
@@ -1,8 +1,10 @@
 package com.effeta.miparroquiaandroid.services.firebase
 
+import com.effeta.miparroquiaandroid.models.Announcement
 import com.effeta.miparroquiaandroid.models.Church
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestore
+import com.effeta.miparroquiaandroid.models.Parish
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.*
 import io.reactivex.Observable
 import javax.inject.Inject
 
@@ -22,7 +24,7 @@ class FirebaseChurchMap @Inject constructor(){
                 if (task.isSuccessful) {
                     val list = ArrayList<Church>()
                     task.result.documents.forEach({ documentSnapshot ->
-                        val a = documentSnapshot.toObject(Church::class.java)
+                        val a = parseChurch(documentSnapshot)
                         a.mKey = documentSnapshot.id
                         list.add(a)
                     })
@@ -35,4 +37,33 @@ class FirebaseChurchMap @Inject constructor(){
         }
     }
 
+    fun getAnnouncementListByParish(parishKey: String?): Observable<List<Church>> {
+        return Observable.create {
+            if (!parishKey.isNullOrEmpty()) {
+                val q: Query = churches.whereEqualTo(Church.FirebaseProperties.parish, parishKey)
+                q.get().addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        it.onNext(parseChurches(task))
+                        it.onComplete()
+                    } else {
+                        it.onError(task.exception!!)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun parseChurch(result: DocumentSnapshot): Church {
+        return result.toObject(Church::class.java)
+    }
+
+    private fun parseChurches(task: Task<QuerySnapshot>): List<Church> {
+        val list = ArrayList<Church>()
+        task.result.documents.forEach({ documentSnapshot ->
+            val a = documentSnapshot.toObject(Church::class.java)
+            a.mKey = documentSnapshot.id
+            list.add(a)
+        })
+        return list
+    }
 }
