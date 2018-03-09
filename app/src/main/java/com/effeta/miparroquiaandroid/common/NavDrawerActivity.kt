df51e19d415c896dfcec2c0fd1ef3c7a70fa7306
Jose Andres Jimenez Porras <jjimenez@soin.co.cr>
2018-03-09 13:26:30 -0600

    fix the nav drawer null point exeption

--- app/src/main/java/com/effeta/miparroquiaandroid/common/NavDrawerActivity.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/common/NavDrawerActivity.kt
@@ -9,19 +9,21 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.effeta.miparroquiaandroid.R
 import com.effeta.miparroquiaandroid.models.Parish
 import com.effeta.miparroquiaandroid.viewmodel.ParishViewModel
 import com.effeta.miparroquiaandroid.views.activities.SelectChurchActivity
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.nav_drawer_header.*
+import kotlinx.android.synthetic.main.nav_drawer_header.view.*
 import org.jetbrains.anko.clearTop
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.singleTop
 import org.jetbrains.anko.toast
 import javax.inject.Inject
 
-abstract class NavDrawerActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+abstract class NavDrawerActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -32,20 +34,43 @@ abstract class NavDrawerActivity : BaseActivity(), NavigationView.OnNavigationIt
 
     abstract val mTitle: Int?
 
+    private lateinit var header: View
+
+    abstract val mLayout: Int
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(mLayout)
+        //Init the Models and UI
 
+        //Set the Nav Drawer
         setSupportActionBar(toolbar)
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
         nav_view.setNavigationItemSelectedListener(this)
-
+        header = nav_view.getHeaderView(0)
         supportActionBar?.title = getString(mTitle!!)
+
+        //Init the view models and UI
+        initialize(false)
+
+        //Set the Observer
+        observeLiveDataToNavDawer(savedInstanceState == null)
+
     }
+
+    private fun initialize(isNewActivity: Boolean) {
+        initializeViewModelsNavDrawer()
+        initializeViewModels()
+        initializeUI()
+    }
+
+    abstract fun initializeUI()
+
+    abstract fun initializeViewModels()
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
@@ -83,21 +108,22 @@ abstract class NavDrawerActivity : BaseActivity(), NavigationView.OnNavigationIt
 
     private fun setNavDrawerInformation(parish: Parish?) {
         parish?.let {
-            textview_email.text = parish.mEmail
-            textview_name.text = parish.mName
-            textview_phonenumber.text = parish.mPhone
-            textview_schedule.text = parish.mSchedule
+            header.textview_email?.text = parish.mEmail
+            header.textview_name?.text = parish.mName
+            header.textview_phonenumber?.text = parish.mPhone
+            header.textview_schedule?.text = parish.mSchedule
         }
     }
 
-    override fun initializeViewModels() {
+    private fun initializeViewModelsNavDrawer() {
         mParishViewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(ParishViewModel::class.java)
     }
 
-    override fun observeLiveData(isNewActivity: Boolean) {
-        mParishViewModel.getParish().observe(this, Observer {
-            setNavDrawerInformation(it)
+    private fun observeLiveDataToNavDawer(isNewActivity: Boolean) {
+        mParishViewModel.getParish().observe(this, Observer<Parish> {
+            setNavDrawerInformation(it!!)
         })
     }
+
 }
