ceaab98d9554f8385f1a76a7cf96b2a07b2a2e09
Jose Andres Jimenez Porras <jjimenez@soin.co.cr>
2018-03-12 18:13:08 -0600

    add simple detail map activity

--- app/src/main/java/com/effeta/miparroquiaandroid/views/activities/DetailMapActivity.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/views/activities/DetailMapActivity.kt
@@ -3,11 +3,19 @@ package com.effeta.miparroquiaandroid.views.activities
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.content.ContextCompat
 import com.effeta.miparroquiaandroid.R
 import com.effeta.miparroquiaandroid.common.BaseActivity
 import com.effeta.miparroquiaandroid.common.EXTRA_CHURCH
 import com.effeta.miparroquiaandroid.models.Church
+import com.effeta.miparroquiaandroid.utils.MapUtils
 import com.effeta.miparroquiaandroid.viewmodel.DetailMapViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_detail_map.*
 import javax.inject.Inject
 
@@ -15,12 +23,14 @@ import javax.inject.Inject
  * Created by jjimenez on 3/12/18.
  */
 
-class DetailMapActivity : BaseActivity() {
+class DetailMapActivity : BaseActivity(), OnMapReadyCallback {
 
     override val mLayout: Int = R.layout.activity_detail_map
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var mMap: GoogleMap
 
 
     private lateinit var mDetailMapViewModel: DetailMapViewModel
@@ -30,12 +40,16 @@ class DetailMapActivity : BaseActivity() {
     }
 
     override fun initializeUI() {
-        if (intent.hasExtra(EXTRA_CHURCH)) {
-            mDetailMapViewModel.setChurchInformation(intent.extras.get(EXTRA_CHURCH) as Church)
-        }
         setSupportActionBar(toolbar_detail)
         toolbar_detail.title = getString(R.string.detail_map_activity_title)
         toolbar_detail.setNavigationOnClickListener { finish() }
+
+        initMapFragment()
+    }
+
+    private fun initMapFragment() {
+        val mapFragment = map as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun observeLiveData() {
@@ -44,11 +58,27 @@ class DetailMapActivity : BaseActivity() {
         })
     }
 
-
-    fun showChurchInformation(church: Church) {
-
+    private fun showChurchInformation(church: Church) {
         church_name.text = String.format(getString(R.string.map_label_church), church.mName)
-
+        addPointToMap(church.mLatitude!!, church.mLongitude!!, church.mName)
     }
 
+    private fun addPointToMap(latitude:Double, longitude:Double, name:String){
+        val pointToAdd = LatLng(latitude, longitude)
+        mMap.addMarker(MarkerOptions()
+                .icon(MapUtils.getMarkerIconFromDrawable(ContextCompat.getDrawable(this, R.drawable.marker_map_church)))
+                .position(pointToAdd)
+                .title(String.format(getString(R.string.map_label_church), name)))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pointToAdd, 17F))
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        if (intent.hasExtra(EXTRA_CHURCH)) {
+            mDetailMapViewModel.setChurchInformation(intent.extras.get(EXTRA_CHURCH) as Church)
+        }
+
+    }
 }
