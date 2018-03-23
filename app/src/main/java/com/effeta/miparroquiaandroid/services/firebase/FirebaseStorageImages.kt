2dabcdc48021e60a24647b564690cc765928a167
Jose Andres Jimenez Porras <jjimenez@soin.co.cr>
2018-03-23 13:22:21 -0600

    make minimal changes

--- app/src/main/java/com/effeta/miparroquiaandroid/services/firebase/FirebaseStorageImages.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/services/firebase/FirebaseStorageImages.kt
@@ -1,6 +1,5 @@
 package com.effeta.miparroquiaandroid.services.firebase
 
-import com.effeta.miparroquiaandroid.common.IMAGES_STORAGE_REFERENCE
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import javax.inject.Inject
@@ -11,7 +10,7 @@ import javax.inject.Inject
 class FirebaseStorageImages @Inject constructor() {
     fun getReferenceStorageImage(churchKey: String): StorageReference {
         val storageRef = FirebaseStorage.getInstance().reference
-        val imagesRef = storageRef.child(IMAGES_STORAGE_REFERENCE)
-        return imagesRef.child(churchKey+".jpg")
+        val imagesRef = storageRef.child("images/churches/")
+        return imagesRef.child(churchKey + ".jpg")
     }
 }
--- app/src/main/java/com/effeta/miparroquiaandroid/viewmodel/DetailMapViewModel.kt
+++ app/src/main/java/com/effeta/miparroquiaandroid/viewmodel/DetailMapViewModel.kt
@@ -3,6 +3,7 @@ package com.effeta.miparroquiaandroid.viewmodel
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.effeta.miparroquiaandroid.models.Church
+import com.effeta.miparroquiaandroid.repositories.ChurchRepository
 import com.effeta.miparroquiaandroid.services.firebase.FirebaseStorageImages
 import com.google.firebase.storage.StorageReference
 import javax.inject.Inject
@@ -10,14 +11,14 @@ import javax.inject.Inject
 /**
  * Created by jjimenez on 3/12/18.
  */
-class DetailMapViewModel @Inject constructor(private val firebaseStorage: FirebaseStorageImages) : ViewModel() {
+class DetailMapViewModel @Inject constructor(private val churchRepository: ChurchRepository) : ViewModel() {
 
     var mChurch: MutableLiveData<Church> = MutableLiveData()
 
     private var mImageChurch: MutableLiveData<StorageReference> = MutableLiveData()
 
     fun getImageChurch(churchKey: String): MutableLiveData<StorageReference> {
-        mImageChurch.value = firebaseStorage.getReferenceStorageImage(churchKey)
+        mImageChurch.value = churchRepository.getChurchImage(churchKey)
         return mImageChurch
 
     }
